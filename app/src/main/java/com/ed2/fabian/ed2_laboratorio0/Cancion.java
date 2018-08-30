26fd992d9cd43d6e40114e931584ea07ddccd8ac
Fabian Alvarez <fabianalvarez23@gmail.com>
2018-08-30 08:15:26 -0600

    Agregando XMLS

---
+++ app/src/main/java/com/ed2/fabian/ed2_laboratorio0/Cancion.java
@@ -0,0 +1,42 @@
+package com.ed2.fabian.ed2_laboratorio0;
+
+public class Cancion {
+
+    private String nombre;
+    private int duracion;
+    private String album;
+    private String artista;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+}
--- app/src/main/java/com/ed2/fabian/ed2_laboratorio0/MainActivity.java
+++ app/src/main/java/com/ed2/fabian/ed2_laboratorio0/MainActivity.java
@@ -2,6 +2,8 @@ package com.ed2.fabian.ed2_laboratorio0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView listado_canciones;
+
+        ArrayAdapter<String> adaptador;
+
+        listado_canciones = (ListView)findViewById(R.id.canciones);
+
+        adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
+        listado_canciones.setAdapter(adaptador);
+
     }
 }
